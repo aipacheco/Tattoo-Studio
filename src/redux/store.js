7df2cd21734e1ecbd8dd6a752057f3a99e09ba84
Ana Pacheco <114119384+Apacheco82@users.noreply.github.com>
2024-03-27 12:08:01 +0100

    fixed persistency

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,38 @@
 import { configureStore } from "@reduxjs/toolkit"
 import authReducer from "./authSlice"
+import { combineReducers } from "redux"
+import storage from "redux-persist/lib/storage"
+import { persistReducer, persistStore } from "redux-persist"
+
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  // añadir más reducers si los hay
+})
+
+// configuración de redux-persist
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth"],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    //resto de reducers, si los hay
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [
+        "persist/PERSIST",
+        "persist/REHYDRATE",
+        "persist/PAUSE",
+        "persist/PURGE",
+        "persist/REGISTER",
+      ],
+    },
+  }),
 })
+
+export const persistor = persistStore(store)

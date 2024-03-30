720940205d8eb603aa66753bf1fa0fe9b785498a
Ana Pacheco <114119384+Apacheco82@users.noreply.github.com>
2024-03-30 09:10:27 +0100

    fixed appointment errors

--- src/pages/Appointment/Appointment.jsx
+++ src/pages/Appointment/Appointment.jsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { useSelector } from "react-redux"
 import { GetServices, PostAppointment } from "../../services/services"
+import Spinner from "../../components/Spinner/Spinner"
+import Alert from "../../components/Alert/Alert"
 
 const Appointment = () => {
   const navigate = useNavigate()
@@ -17,11 +19,16 @@ const Appointment = () => {
     service_id: 1,
   })
   const [services, setServices] = useState([])
+  const [stateMessage, setStateMessage] = useState({
+    message: "",
+    className: "",
+  })
+  const [alert, setAlert] = useState(false)
 
   const fetchData = async () => {
     const allServices = await GetServices()
     setServices(allServices.data)
-    console.log(services)
+    // console.log(services)
   }
 
   useEffect(() => {
@@ -45,13 +52,29 @@ const Appointment = () => {
     e.preventDefault()
     setLoading(true)
     try {
-      //falta la parte de pintar en pantalla los mensajes y errores
-      console.log(appointment)
+      // console.log(appointment)
       const postAppointment = await PostAppointment(appointment, token)
-      console.log(postAppointment)
+      if (postAppointment.success) {
+        setAlert(true)
+        setStateMessage({
+          message:"Cita creada correctamente",
+          className: "success",
+        })
+        setInterval(() => {
+          setAlert(false)
+          navigate("/profile")
+        }, 1500)
+      }
     } catch (error) {
+      setLoading(false)
+      setAlert(true)
+      setStateMessage({
+        message: `${error}`,
+        className: "danger",
+      })
       console.log(error)
     }
+    setLoading(false)
   }
 
   const handleServiceChange = ({ target }) => {
@@ -63,35 +86,49 @@ const Appointment = () => {
 
   return (
     <>
-      <div className="container d-flex justify-content-center mt-2">
-        <div className="col-12 col-md-6 col-lg-6">
-          <h1 className="d-flex justify-content-center">
-            Selecciona fecha y hora
-          </h1>
-          <Datetime onDateTimeChange={handleDateTimeChange} />
-          <div className="d-flex justify-content-center m-3">
-            <select
-              className="form-select"
-              onChange={handleServiceChange}
-              value={appointment.service_id}
-            >
-              {/* <option value="" disabled selected>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <div className="container d-flex justify-content-center mt-2">
+            <div className="col-12 col-md-6 col-lg-6">
+              <h1 className="d-flex justify-content-center">
+                Selecciona fecha y hora
+              </h1>
+              {alert && (
+                <div className="d-flex justify-content-center mt-3">
+                  <Alert
+                    className={stateMessage.className}
+                    message={stateMessage.message}
+                  />
+                </div>
+              )}
+              <Datetime onDateTimeChange={handleDateTimeChange} />
+              <div className="d-flex justify-content-center m-3">
+                <select
+                  className="form-select"
+                  onChange={handleServiceChange}
+                  value={appointment.service_id}
+                >
+                  {/* <option value="" disabled selected>
                 Selecciona el servicio
               </option> */}
-              {services.map((service) => (
-                <option key={service.id} value={service.id}>
-                  {service.serviceName}
-                </option>
-              ))}
-            </select>
+                  {services.map((service) => (
+                    <option key={service.id} value={service.id}>
+                      {service.serviceName}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <Button
-        text={"Pedir cita"}
-        isFormComplete={isFormComplete}
-        handleSubmit={handleSubmit}
-      />
+          <Button
+            text={"Pedir cita"}
+            isFormComplete={isFormComplete}
+            handleSubmit={handleSubmit}
+          />
+        </>
+      )}
     </>
   )
 }

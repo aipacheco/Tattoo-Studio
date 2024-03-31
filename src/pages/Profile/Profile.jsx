abdf0b3d01aa826593d9708959108e45b49fc233
Ana Pacheco <114119384+Apacheco82@users.noreply.github.com>
2024-03-31 17:33:56 +0200

    feat: profile delete button, pending functionality

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -9,6 +9,7 @@ import ProfileCard from "../../components/ProfileCard/ProfileCard"
 import DataTable from "react-data-table-component"
 import LinkButton from "../../components/LinkButton/LinkButton"
 import { format, addHours } from "date-fns"
+import { Modal } from "reactstrap"
 
 const Profile = () => {
   const navigate = useNavigate()
@@ -16,6 +17,8 @@ const Profile = () => {
   const [loading, setLoading] = useState(false)
   const [profile, setProfile] = useState([])
   const [appointments, setAppointments] = useState([])
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [selectedAppointment, setSelectedAppointment] = useState(null)
 
   const fetchProfile = async () => {
     setLoading(true)
@@ -53,12 +56,34 @@ const Profile = () => {
       selector: (row) => formatDate(row.appointment_date),
       sortable: true,
     },
+    {
+      name: "Eliminar",
+      button: true,
+      cell: (row) => (
+        <button
+          className="btn btn-outline-light"
+          onClick={() => handleDeleteClick(row)}
+        >
+          <i className="material-symbols-outlined">cancel</i>
+        </button>
+      ),
+    },
   ]
   const handleSelect = (state) => {
     // para acceder a las filas seleccionadas
     console.log(state.selectedRows)
   }
+  const handleDeleteClick = (appointment) => {
+    //lo setea a selectedAppointment
+    setSelectedAppointment(appointment)
+    console.log(appointment)
+    setIsModalOpen(true)
+  }
 
+  const handleConfirmDelete = () => {
+    console.log("Confirmar")
+    console.log(selectedAppointment)
+  }
   const { avatar, email, first_name, last_name } = profile
 
   return (
@@ -67,6 +92,23 @@ const Profile = () => {
         <Spinner />
       ) : (
         <>
+          <Modal className="center-modal" isOpen={isModalOpen}>
+            <h2>Confirmar Eliminación</h2>
+            <p>¿Estás seguro de que deseas eliminar esta cita?</p>
+            <button
+              className="btn btn-outline-light"
+              onClick={handleConfirmDelete}
+            >
+              Confirmar
+            </button>
+            <button
+              className="btn btn-outline-light"
+              onClick={() => setIsModalOpen(false)}
+            >
+              Cancelar
+            </button>
+          </Modal>
+
           <div className=" mt-3 mb-3">
             <ProfileCard
               avatar={avatar}
@@ -75,9 +117,10 @@ const Profile = () => {
               email={email}
             />
           </div>
-          <div className=" mt-3 mb-3">
+
+          <div className="mt-3 mb-3">
             <h1 className="center-flex">Tus citas</h1>
-            <div className="container container-fluid">
+            <div className="center-flex p-3">
               <DataTable
                 columns={columns}
                 data={appointments}
@@ -86,6 +129,7 @@ const Profile = () => {
                 onSelectedRowsChange={handleSelect}
               />
             </div>
+
             <div className="center-flex mt-3 mb-3">
               <LinkButton
                 direction={"/appointment"}
